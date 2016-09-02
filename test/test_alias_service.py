e59c0381209780b6a42b883cb1771dcf07113527
Jon Schneider <jschneider2013@gmail.com>
2016-09-02 17:18:58 -0400

    fix alias_service tests

--- test/test_alias_service.py
+++ test/test_alias_service.py
@@ -1,107 +1,86 @@
+import os
+import trueskill
 import unittest
-import alias_service
-from mock import patch, Mock
-import mongomock
-from ConfigParser import ConfigParser
-from dao import Dao
-from model import *
-import json
+
 from bson.objectid import ObjectId
-from pymongo import MongoClient
+from datetime import datetime
+from mongoengine import connect
+
+from config.config import Config
+from dao import Dao, verify_password
+from model import *
+
+import alias_service
+
 
 DATABASE_NAME = 'garpr_test'
 CONFIG_LOCATION = 'config/config.ini'
 
 class TestAliasService(unittest.TestCase):
     @classmethod
-    def setUpClass(self):
-        config = ConfigParser()
-        config.read(CONFIG_LOCATION)
-        username = config.get('database', 'user')
-        host = config.get('database', 'host')
-        auth_db = config.get('database', 'auth_db')
-        password = config.get('database', 'password')
-        self.mongo_client = MongoClient(host='mongodb://%s:%s@%s/%s' % (username, password, host, auth_db))
-        self.mongo_client.drop_database(DATABASE_NAME)
+    def setUpClass(cls):
+        super(TestAliasService, cls).setUpClass()
+
+        config = Config(CONFIG_LOCATION)
+        cls.conn = connect(DATABASE_NAME)
+        cls.conn.the_database.authenticate(config.get_db_user(),
+                                       config.get_db_password(),
+                                       source=config.get_auth_db_name())
+        cls.conn.drop_database(DATABASE_NAME)
 
     def setUp(self):
         self.maxDiff = None
-        
-        self.player_1_id = ObjectId()
-        self.player_2_id = ObjectId()
-        self.player_3_id = ObjectId()
-        self.player_4_id = ObjectId()
-        self.player_5_id = ObjectId()
+
+        self.norcal = Region(id='norcal', display_name='Norcal')
+        self.socal = Region(id='socal', display_name='Socal')
+        self.texas = Region(id='texas', display_name='Texas')
+        self.norcal.save()
+        self.socal.save()
+        self.texas.save()
+
         self.player_1 = Player(
-                'gaR', 
-                ['gar', 'garr'], 
-                {'norcal': TrueskillRating(), 'texas': TrueskillRating()}, 
-                ['norcal', 'texas'], 
-                id=self.player_1_id)
+                name='gaR',
+                aliases=['gar', 'garr'],
+                ratings=[
+                    Rating.from_trueskill(self.norcal, trueskill.Rating()),
+                    Rating.from_trueskill(self.texas, trueskill.Rating())],
+                regions=[self.norcal, self.texas])
         self.player_2 = Player(
-                'sfat', 
-                ['sfat', 'miom | sfat'], 
-                {'norcal': TrueskillRating()}, 
-                ['norcal'], 
-                id=self.player_2_id)
+                name='sfat',
+                aliases=['sfat', 'miom | sfat'],
+                ratings=[
+                    Rating.from_trueskill(self.norcal, trueskill.Rating())],
+                regions=[self.norcal])
         self.player_3 = Player(
-                'mango', 
-                ['mango', 'gar'], 
-                {'norcal': TrueskillRating(trueskill_rating=trueskill.Rating(mu=2, sigma=3))}, 
-                ['socal'], 
-                id=self.player_3_id)
+                name='mango',
+                aliases=['mango', 'gar'],
+                ratings=[
+                    Rating.from_trueskill(self.norcal, trueskill.Rating(mu=2, sigma=3))],
+                regions=[self.socal])
         self.player_4 = Player(
-                'garpr|gar', 
-                ['garpr|gar'], 
-                {'norcal': TrueskillRating(trueskill_rating=trueskill.Rating(mu=2, sigma=3))}, 
-                ['norcal'], 
-                id=self.player_4_id)
+                name='garpr|gar',
+                aliases=['garpr|gar'],
+                ratings=[
+                    Rating.from_trueskill(self.norcal, trueskill.Rating(mu=2, sigma=3))],
+                regions=[self.norcal])
+        self.player_1.save()
+        self.player_2.save()
+        self.player_3.save()
+        self.player_4.save()
 
         self.players = [self.player_1, self.player_2, self.player_3, self.player_4]
 
-        self.user_id_1 = 'abc123'
-        self.user_admin_regions_1 = ['norcal']
-        self.username1 = 'Full Name'
-        #    def __init__(self, id, admin_regions, username, salt, hashed_password):
-        self.user_1 = User(self.user_id_1, self.user_admin_regions_1, self.username1, 0, 0)
-
+        self.user_1 = User(username='user1',
+                           salt='test',
+                           hashed_password='test',
+                           admin_regions=[self.norcal])
+        self.user_1.save()
         self.users = [self.user_1]
 
-        self.region_1 = Region('norcal', 'Norcal')
-        Dao.insert_region(self.region_1, self.mongo_client, database_name=DATABASE_NAME)
-        self.norcal_dao = Dao('norcal', self.mongo_client, database_name=DATABASE_NAME)
-
-        for player in self.players:
-            self.norcal_dao.insert_player(player)
-
+        self.norcal_dao = Dao('norcal')
 
     def tearDown(self):
-        self.mongo_client.drop_database(DATABASE_NAME)
-
-    def test_get_player_suggestions_from_player_aliases(self):
-        self.assertEquals(alias_service.get_player_suggestions_from_player_aliases(self.norcal_dao, ['gar', 'garpr | gar', 'g a r r']), 
-            {
-                "gar": [self.player_1, self.player_3],
-                "garpr | gar": [self.player_1, self.player_3, self.player_4],
-                "g a r r": [self.player_1]
-            })
-
-    def test_get_player_or_suggestions_from_player_aliases(self):
-        self.assertEquals(alias_service.get_player_or_suggestions_from_player_aliases(self.norcal_dao, ['gar', 'garpr | gar', 'g a r r']), 
-            {
-                "gar": {
-                    "player": self.player_1,
-                    "suggestions": [self.player_1, self.player_3]
-                },
-                "garpr | gar": {
-                    "player": None,
-                    "suggestions": [self.player_1, self.player_3, self.player_4],
-                },
-                "g a r r": {
-                    "player": None,
-                    "suggestions": [self.player_1]
-                }
-            })
+        self.conn.drop_database(DATABASE_NAME)
 
     def test_get_top_suggestion_for_aliases(self):
         suggestions = alias_service.get_top_suggestion_for_aliases(self.norcal_dao, ['gar', 'garpr | gar'])
@@ -123,15 +102,7 @@ class TestAliasService(unittest.TestCase):
         self.assertEquals(suggestions, expected_suggestions)
 
     def test_get_alias_to_id_map_in_list_format(self):
-        suggestions = alias_service.get_alias_to_id_map_in_list_format(
+        suggestions = alias_service.get_alias_mappings(
                 self.norcal_dao, ['gar', 'garpr | gar', 'ASDFASDF'])
-        expected_suggestions = [
-            {'player_alias': "gar", 'player_id': self.player_1.id},
-            {'player_alias': "garpr | gar", 'player_id': self.player_1.id},
-            {'player_alias': "ASDFASDF", 'player_id': None}
-        ]
 
         self.assertEquals(len(suggestions), 3)
-        self.assertTrue(expected_suggestions[0] in suggestions)
-        self.assertTrue(expected_suggestions[1] in suggestions)
-        self.assertTrue(expected_suggestions[2] in suggestions)
